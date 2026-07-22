0c446dbb149ae7392b26ff3b31b2c4e52f497f6c
Shantanu Goel <shantanu.goel@gmail.com>
2026-07-23 00:33:07 +0530

    Initial Commit

---
+++ Atarang/AtarangApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtarangApp.swift
+//  Atarang
+//
+//  Created by Shantanu Goel on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AtarangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
